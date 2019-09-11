c15c27ca3c6a02ec2710e31f36ea82f786c010b1
Administrator <576471639@qq.com>
2019-09-11 21:42:30 +0800

    2019.9.11 21:20

---
+++ 运算符.py
@@ -0,0 +1,25 @@
+# 算术运算符 + - * / %(求余数) **(求幂) //(求商的整数)
+print(1 + 3)
+print(1.0 + 3)
+print(1 * 3)
+print(1 - 3)
+print(1 / 3)
+print(1 % 3)
+print(1 // 3)
+print(2 ** 3)
+
+# 比较运算符 == 比较是否相等 != > < >= <=  返回布尔值
+# 布尔运算 and or not
+a = 0
+b = 20
+c = (a and b)  # 0 a为0的时候作布尔运算为假  大于0都为真
+d = a or b  # 20 一真为真
+e = not(a and b)  # true
+print(a and b)
+
+a = 30
+b = 20
+c = (a and b)  # 30 a为0的时候作布尔运算为假  大于0都为真
+d = a or b  # 20 一真为真
+e = not(a and b)  # false
+
