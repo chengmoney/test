e8bc3d7e0e858d57208f2bae4365d11cf60ee17d
Administrator <576471639@qq.com>
2019-09-14 21:31:16 +0800

    2019.9.14 pm:9:30

--- list.py
+++ list.py
@@ -22,6 +22,130 @@ len(a)  # 长度为3
 a[len(a):] = ['ab', 35]
 
 # 合并list  list.extend(L) Extend the list by appending all the items in the given list;
+la = [1, 3, 56]
+lb = ['a', 'b', 'abc']
+la.extend(lb)
+print(la)
+
+text = 'avc'
+num = 5
+la.extend(text)
+print(la)  # [1, 3, 56, 'a', 'b', 'abc', 'a', 'v', 'c']  text以字符为单位拆开添加进入la中
+la.extend(num)  # 不能添加数字，会报错  TypeError: 'int' object is not iterable
+
+# 试一试添加汉字
+lb = [1, 3, 5]
+text = '我是成'
+lb.extend(text)
+print(lb)  # [1, 3, 5, '我', '是', '成']
+
+# list.extend and list.append 的区别  append是成建制的追加，extend是个体化扩编
+la = [1, 2, 3]
+lb = [4, 5]
+la.append(lb)  # [1, 2, 3, [4, 5]]
+
+la = [1, 2, 3]
+lb = [4, 5]
+la.extend(lb)
+print(la)  # [1, 2, 3, 4, 5]
+
+la = [1, 2, 3]
+lb = [4, 5]
+la[len(la):] = [4, 5]
+print(la)  # [1, 2, 3, 4, 5]
+
+la = [1, 2, 3]
+lb = [4, 5]
+la[len(la):len(la) + 1] = [4, 5]
+print(la)  # [1, 2, 3, 4, 5]
+
+la = []
+text = '0123456789'
+la.append(text)  # ['0123456789']
+
+la = []
+text = '123456789'
+la.extend(text)
+print(la)  # ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+print(la[1:3])  # 从0开始编码，截取la的2-3位。用la[a:b] a表示从哪一位开始，
+# b表示从哪一位截止，截止的位数不包含b位
+
+# list 中某元素个个数 list.count(x) 返回值是一个数字
+lt = [1, 1, 1, 3, 4, '1']
+a = lt.count(1)
+b = lt.count(2)
+c = lt.count(3)
+d = lt.count('1')
+type(a)  # <class 'int'>
+print(a, b, c, d)  # 3 0 1 1
+
+# 元素在list中的位置 list.index()
+# Return the index in the list of the first item whose value is x. It is an error if there is no such item.
+
+lt = [1, 2, 3, 4, 1, 5]
+a = lt.index(1)
+type(a)  # <class 'int'> 返回值是int
+print(a)  # 0  list中第一次出现1的元素位置
+b = lt.index(3)
+print(b)  # 2
+c = lt.index(6)  # ValueError: 6 is not in list 报错
+
+# 向list中插入一个元素（指定位置插入） list.insert(i, x)
+# Insert an item at a given position. The first argument(参数） is the index of the element before which to insert
+# so a.insert(0, x) inserts at the front of the list, and a.insert(len(a), x) is equivalent to a.append(x).
+
+lt = ['a', 'b', 'c', 'd']
+lt.insert(0, 'aa')  # 在list第一个元素前插入一个元素
+lt.insert(2, 'a')  # 在list第3个元素前插入一个元素
+lt.insert(len(lt), 'a')  # 在list最后追加一个元素
+lt.insert(len(lt), ['ab', 'ac'])  # ==lt.append(['ab', 'ac']
+print(lt)  # ['aa', 'a', 'a', 'b', 'c', 'd', 'a', ['ab', 'ac']]
+
+# list的删除  list.remove(x)
+# Remove the first item from the list whose value is x. It is an error if there is no such item
+# list.pop([i])
+# Remove the item at the given position in the list, and return it. If no index is specified, a.pop() removes and
+# returns the last item in the list.
+lt = ['aa', 'a', 'a', 'b', 'c', 'd', 'a']
+lt.remove('a')  # 删除了list中第一个内容是'a'的值。不会删除所有的内容是'a'的值
+print(lt)  # ['aa', 'a', 'b', 'c', 'd', 'a']
+lt.remove("1")  # ValueError: list.remove(x): x not in list
+
+lt = ['aa', 'a', 'a', 'b', 'c', 'd', 'a']
+c = lt.pop(3)  # 删除list中第4个元素，且将删除的'b'储存在c变量中
+print(c)
+print(lt)
+
+# 实验list.remove()有没有返回值
+lt = ['aa', 'a', 'a', 'b', 'c', 'd', 'a']
+d = lt.remove('a')
+print(d)  # None  不产生返回值
+
+#   判断一个元素在不在list中用 a in list
+lt = ['aa', 'a', 'a', 'b', 'c', 'd', 'a']
+lt.append(['a', 'a'])
+print('a' in lt)  # 产生一个返回值true
+print(['a', 'a'] in lt)  # 产生一个返回值true
+print(['a', 'b'] in lt)  # 产生一个返回值false 说明in只能判断一个值或者子list是否在list中
+
+# 判断某个元素在list中，然后删除
+lt = ['aa', 'a', 'a', 'b', 'c', 'd', 'a']
+ind = input("请输入想要查询的元素")
+if ind in lt:
+    lt.remove(ind)
+    print(lt)
+else:
+    print('"%s"元素不在list中' % ind)
+
+
+
+
+
+
+
+
+
+
 
 
 
