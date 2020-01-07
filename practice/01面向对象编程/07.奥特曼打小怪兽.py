827bbea5991e00e2bd22e022959faff42831bb53
Administrator <576471639@qq.com>
2020-01-07 23:32:05 +0800

    cy.2020.1.7

--- practice/01面向对象编程/07.奥特曼打小怪兽.py
+++ practice/01面向对象编程/07.奥特曼打小怪兽.py
@@ -66,9 +66,13 @@ class Ultraman(Fighter):
         super().__init__(name, hp, attack_power)
         self._mp = mp
 
-    def normal_attack(self, other):
+    @property
+    def mp(self):
+        return self._mp
+
+    def attack(self, other):
         attack_hp = randint(self.attack_power * 0.4, self.attack_power)
-        print('对{}发起普通{}攻击'.format(other.name, attack_hp))
+        print('{}对{}发起普通{}攻击'.format(self.name, other.name, attack_hp))
         other.hp -= attack_hp
 
     def magic_attack(self, others):
@@ -79,7 +83,10 @@ class Ultraman(Fighter):
         """
         if self._mp > 20:
             for other in others:
-                other.hp -= self.attack_power
+                if other.alive:
+                    other.hp -= self.attack_power
+                    print('{}对{}发起{}魔法攻击'.format(self.name, other.name, self.attack_power))
+            self._mp -= 20
             return True
         else:
             return False
@@ -104,7 +111,12 @@ class Ultraman(Fighter):
             print('对{}发起1倍暴击{}攻击'.format(other.name, attack))
             other.hp -= attack
         else:
-            self.normal_attack(other)
+            self.attack(other)
+
+    def __str__(self):
+        return '~~~{}奥特曼~~~\n'.format(self.name) + \
+                '生命值{}'.format(self._hp) + \
+                '魔法值{}'.format(self._mp)
 
 
 class Master(Fighter):
@@ -114,12 +126,75 @@ class Master(Fighter):
     def attack(self, other):
         other.hp -= self.attack_power
 
+    def __str__(self):
+        return '~~~{}小怪兽~~~\n'.format(self.name) + \
+                '生命值{}'.format(self._hp)
 
-ut = Ultraman('cy', 400, 100, 5)
-mt = Master('hh', 200, 10)
 
-while mt.alive:
-    ut.critical_attack(mt)
+def is_any_alive(fighters):
+    for fighter in fighters:
+        if fighter.hp > 0:
+            return True
+    return False
+
+
+def select_master(masters):
+    """随机选择一个存活的怪兽"""
+    num = len(masters)
+    while True:
+        index = randrange(num)
+        master = masters[index]
+        if master.hp > 0:
+            return master
+
+
+def display_info(ultm, masters):
+    print(ultm)
+    for master in masters:
+        print(master)
+
+
+def main():
+    u = Ultraman('迪加', 200, 50, 10)
+    m1 = Master('皮卡丘', 50, 5)
+    m2 = Master('妙蛙种子', 40, 10)
+    m3 = Master('喵喵兽', 20, 15)
+    ms = [m1, m2, m3]
+    fight_round = 1
+    display_info(u, ms)
+    while u.alive and is_any_alive(ms):
+        print('-----------第{}回合----------'.format(fight_round))
+        m = select_master(ms)
+        skill = randint(1, 10)
+        if skill <= 5:
+            u.critical_attack(m)
+        else:
+            if u.magic_attack(ms):
+                print('{}对怪兽们发动了魔法攻击'.format(u.name))
+        for master in ms:
+            if master.alive:
+                master.attack(u)
+                print('{}对{}发动了攻击,攻击{}点血'.format(master.name, u.name, master.attack_power))
+        fight_round += 1
+        display_info(u, ms)
+
+    print('-----------战斗结束-----------')
+    if u.alive:
+        print('奥特曼胜利')
+    else:
+        print('怪兽胜利')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
 
 
 
